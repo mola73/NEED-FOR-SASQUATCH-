5d65b804120a387cbba1f2019f4f030421ebfc0e
stuart <ganza@unbc.ca>
2024-10-11 15:13:13 -0700

    initial commit

---
+++ app/src/main/java/com/example/needforsasquatch/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.needforsasquatch;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // start menu music
+        mediaPlayer = MediaPlayer.create(this, R.raw.start_menu_music);
+        mediaPlayer.setLooping(true);  // Loop music
+        mediaPlayer.start();
+
+        //start ImageButton
+        ImageButton startButton = findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the Mode Selection Screen
+                Intent intent = new Intent(MainActivity.this, ModeSelectionActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/needforsasquatch/ModeSelectionActivity.java
@@ -0,0 +1,14 @@
+package com.example.needforsasquatch;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ModeSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mode_selection);
+    }
+}
+
