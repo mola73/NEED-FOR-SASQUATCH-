a03bb879e9c20cfb81dac210b425a2239efdbb0f
stuart <ganza@unbc.ca>
2024-10-18 15:14:23 -0700

    David Changes 10/18/24

--- app/src/main/java/com/example/needforsasquatch/MainActivity.java
+++ app/src/main/java/com/example/needforsasquatch/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.needforsasquatch;
+
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -8,24 +9,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MediaPlayer mediaPlayer;
+    public static MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // start menu music
+        // Start menu music
         mediaPlayer = MediaPlayer.create(this, R.raw.start_menu_music);
-        mediaPlayer.setLooping(true);  // Loop music
+        mediaPlayer.setLooping(true);
         mediaPlayer.start();
 
-        //start ImageButton
         ImageButton startButton = findViewById(R.id.start_button);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Navigate to the Mode Selection Screen
                 Intent intent = new Intent(MainActivity.this, ModeSelectionActivity.class);
                 startActivity(intent);
             }
